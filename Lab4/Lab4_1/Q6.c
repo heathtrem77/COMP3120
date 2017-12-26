ac4cb0f3acf91da34ed1b46041cc8b186acd78ab
heathtrem77 <jkhb62012@gmail.com>
2017-12-26 16:09:37 +0900

    intermediate checking

--- Lab4/Lab4_1/Q6.c
+++ Lab4/Lab4_1/Q6.c
@@ -8,6 +8,10 @@
 int main() {
 	int i, status;
 	pid_t p[1];
+	char data[200];
+	int pipe1[2];
+
+	pipe(pipe1) == -1;
 
 	p[0] = fork();
 
@@ -16,6 +20,8 @@ int main() {
 
 	else if(p[0] == 0) {//child process
 		printf("child process number: %d\n", getpid());
+		printf("child %d: OK! I sended my letter!\n\n", getpid());
+		write(pipe1[1], "This is for my brother!", sizeof(data));
 	}
 	
 	else {//parent process
@@ -24,11 +30,13 @@ int main() {
 			return -1;
 
 		else if(p[1] == 0) {
+			waitpid(p[0], &status, 0);
+			read(pipe1[0], data, sizeof(data));
 			printf("child process number: %d\n", getpid());
+			printf("child %d: I Received letter (%s) \n\n", getpid(), data);
 		}
 		else
 			printf("I'm parent process %d\n", getpid());
-		wait(&status);
 	}
 	return 0;
 }
